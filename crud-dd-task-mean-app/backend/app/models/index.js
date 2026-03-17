b0d27f2b5a6fdfc62af0e9170bfe152febdffeb0
Ranjeeth Reddy S <109015686+ranjireddy041@users.noreply.github.com>
2026-03-17 14:58:23 +0530

    Update index.js

--- crud-dd-task-mean-app/backend/app/models/index.js
+++ crud-dd-task-mean-app/backend/app/models/index.js
@@ -1,13 +1,14 @@
 const dbConfig = require("../config/db.config.js");
 
 const mongoose = require("mongoose");
+
 mongoose.Promise = global.Promise;
 
 const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.tutorials = require("./tutorial.model.js")(mongoose);
 
-module.exports = {
-  url: process.env.MONGO_URI || "mongodb://mongo:27017/dd_db"
-};
+db.mongoose = mongoose;
+
+// Use Docker environment variable
+db.url = process.env.MONGO_URI || "mongodb://mongodb:27017/dd_db";
+
+module.exports = db;
